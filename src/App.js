9aa05de119d51428309708a59c3a7799c0743881
Chris Kemeza <ckemeza1@gmail.com>
2025-07-03 18:06:55 -0400

    :metal: :fire: :metal:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import './index.css';
+import { TailwindLogo } from './TailwindLogo';
 
 function App() {
+  const [baseSize, setBaseSize] = useState(16);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newSize = parseFloat(inputRef.current.value);
+    if (!isNaN(newSize)) setBaseSize(parseFloat(newSize));
+  };
+
+  const rows = [];
+
+  for (let i = 1; i <= 96; i += 0.5) {
+    const key = parseFloat(i.toFixed(1));
+
+    const label = (key % 1 === 0) ? key.toFixed(0) : key.toFixed(1);
+    const remValue = (i / 4).toFixed(2);
+    const pxValue = parseFloat((i * baseSize).toFixed(1));
+
+    rows.push(
+      <tr key={label} className="text-sm">
+        <td className="class-row">property-{label}</td>
+        <td className="property-row">property: {remValue}rem</td>
+        <td className="value-row">{pxValue}px</td>
+      </tr>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="flex justify-between">
+        <div className="header-wrapper">
+          <TailwindLogo />
+        <h1>tailwindcss sizing chart</h1>
+        </div>
       </header>
+
+      <main>
+        <form onSubmit={handleSubmit}>
+          <div className="form-wrap">
+            <label htmlFor="fontSize">
+              Set Base Font Size (px)
+            </label>
+            <input
+              id="fontSize"
+              className="clean-input"
+              ref={inputRef}
+              defaultValue={baseSize}
+              placeholder="Default 16px"
+              type="text"
+              step="0.1"
+            />
+            <button
+            type="submit"
+            className="bg-[rgb(30,41,59)] text-[rgb(229,231,235)] rounded-md px-4 py-2 hover:opacity-80"
+          >
+            Set Size
+          </button>
+          </div>
+        </form>
+
+        <table className="table-custom">
+          <thead>
+            <tr className="text-left">
+              <th className="text-white py-2 px-2">Class</th>
+              <th className="text-white py-2 px-2">Property</th>
+              <th className="text-white py-2 px-2">Value</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      </main>
     </div>
   );
 }
